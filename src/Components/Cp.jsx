86a2c9388650a5a9e78295e17b05fea815c0e4fd
abimanyu2515 <ctsssabimanyu.s@gmail.com>
2025-10-03 03:22:34 +0530

    Updated CP profiles

--- src/Components/Cp.jsx
+++ src/Components/Cp.jsx
@@ -5,25 +5,29 @@ import { textVariant, fadeIn } from '../utils/motion'
 
 const Cp = () => {
     return (
-        <>
+        <div className=''>
           <motion.div variants={textVariant()}>
             <h2 className={`${styles.sectionHeadText}`}>Competitive Programming.</h2>
           </motion.div>
 
+          <a href="https://leetcode.com/u/Abimanyu_S/">
           <motion.p
             variants={fadeIn("", "", 0.1, 1)}
-            className="mt-4 p-2  border  text-red-400 text-[17px] leading-[30px]"
+            className="mt-4 p-2  border  text-red-400 text-[17px] leading-[30px] hover:bg-red-700 hover:text-white"
           >
             LEETCODE <br /> Secured contest rating of 1450 at the top 62.98% globally and solved 340 problems using Python.
           </motion.p>
+          </a>
 
+          <a href="https://www.codechef.com/users/abi_2504">  
           <motion.p
             variants={fadeIn("", "", 0.1, 1)}
-            className="mt-4 p-2 border  text-blue-500 text-[17px] leading-[30px]"
+            className="mt-4 p-2 border  text-blue-500 text-[17px] leading-[30px] hover:bg-blue-800 hover:text-white"
           >
             CODECHEF <br /> Secured contest rating of 957 along with active participation and have solved over 200 problems.
           </motion.p>
-        </>
+          </a>
+        </div>
     )
 }
 
