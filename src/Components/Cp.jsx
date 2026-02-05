0be01418d8114a4ecc9ab2c5e9ba8c1b05c65ad0
ABIMANYU_S <ctsssabimanyu.s@gmail.com>
2026-02-06 01:19:23 +0530

    update contest rating

--- src/Components/Cp.jsx
+++ src/Components/Cp.jsx
@@ -15,7 +15,7 @@ const Cp = () => {
             variants={fadeIn("", "", 0.1, 1)}
             className="mt-4 p-2  border  text-red-400 text-[17px] leading-[30px] hover:bg-red-700 hover:text-white"
           >
-            LEETCODE <br /> Secured max contest rating of 1508 at the top 41.66% globally and solved 500+ problems using Python.
+            LEETCODE <br /> Secured max contest rating of 1601 at the top 23.17% globally and solved 500+ problems using Python.
           </motion.p>
           </a>
 
@@ -24,7 +24,7 @@ const Cp = () => {
             variants={fadeIn("", "", 0.1, 1)}
             className="mt-4 p-2 border  text-blue-500 text-[17px] leading-[30px] hover:bg-blue-800 hover:text-white"
           >
-            CODECHEF <br /> Secured contest rating of 1231 along with active participation and have solved over 200 problems.
+            CODECHEF <br /> Secured contest rating of 1257 along with active participation and have solved over 200 problems.
           </motion.p>
           </a>
         </div>
